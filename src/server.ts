58b3647834fdc6d15964eb06364494c0eca8b30b
Daniel <d.soares@aluno.ifsp.edu.br>
2021-02-24 22:24:20 -0300

    user controller and model working inserting into database

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,13 @@
 import 'reflect-metadata';
 import express, { request, response } from 'express';
 import "./database";
+import { router } from './routes';
+
 
 const app = express();
+app.use(express.json());
+
 
 app.listen(3333, () => console.log('server is running'));
 
-// http://localhost:3333/
-app.get("/", (request, response) => {
-  return response.json({
-    message: "hello world dnv"
-  })
-});
-
-//1 param: rota
-//2 param: req resp
-app.post("/", (request, response)=>{
-  return response.json({
-    mensagem: "os dados foram salvos com sucesso"
-  })
-});
+app.use(router);
